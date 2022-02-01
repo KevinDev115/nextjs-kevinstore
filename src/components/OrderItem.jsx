a1f318969d61b5eac17a9124960a3b12b427ce08
KevinDev115 <galindok829@gmail.com>
2022-02-01 18:11:21 -0500

    configure cscc  module

--- src/components/OrderItem.jsx
+++ src/components/OrderItem.jsx
@@ -1,7 +1,10 @@
 import React, { useContext } from 'react';
+import Image from 'next/image';
 import AppContext from '@context/AppContext';
 import close from '@icons/icon_close.png';
 import styles from '@styles/OrderItem.module.scss';
+
+const imgNotFound = 'https://media.istockphoto.com/vectors/error-page-or-file-not-found-icon-vector-id924949200?k=20&m=924949200&s=170667a&w=0&h=-g01ME1udkojlHCZeoa1UnMkWZZppdIFHEKk6wMvxrs=';
 
 const OrderItem = ({ product }) => {
   const { removeFromCart } = useContext(AppContext);
@@ -13,11 +16,11 @@ const OrderItem = ({ product }) => {
   return (
     <div className={styles.OrderItem}>
       <figure>
-        <img src={product.images[0]} alt={product.title} />
+        <Image src={product?.images[0] || imgNotFound} alt={product?.title} width={240} height={240} />
       </figure>
-      <p>{product.title}</p>
-      <p>${product.price}</p>
-      <img className={(styles.pointer, styles['more-clickable-area'])} src={close} alt="close" onClick={() => handleRemove(product)} />
+      <p>{product?.title}</p>
+      <p>${product?.price}</p>
+      <Image className={(styles.pointer, styles['more-clickable-area'])} src={close} alt="close" onClick={() => handleRemove(product)} />
     </div>
   );
 };
