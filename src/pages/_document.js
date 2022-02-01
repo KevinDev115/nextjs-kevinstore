81c4c05bf6b29779d86945970cce1fadb728517b
KevinDev115 <galindok829@gmail.com>
2022-02-01 18:50:43 -0500

    config how build product

---
+++ src/pages/_document.js
@@ -0,0 +1,31 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <title>Next Shop</title>
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-RE3D04G2WD"></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+
+                gtag('config', 'G-RE3D04G2WD');
+              `,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
